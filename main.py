f5c06c62533a6bdeadc28c30a4b5ba1125dab736
mura05251962-GitH <mura05251962@gmail.com>
2026-03-20 18:50:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ st.markdown("""
 
 /* ===== 共通フォントサイズ（全体を大きく） ===== */
 html, body, [class*="css"] {
-    font-size: 22px !important;
+    font-size: 24px !important;
     font-family: "メイリオ", Meiryo, sans-serif !important;
 }
 /* すべての文字色を黒にする */
