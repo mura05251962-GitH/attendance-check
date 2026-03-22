d40ec56356df904a0f548804f4025efde0573fe7
mura05251962-GitH <mura05251962@gmail.com>
2026-03-22 22:21:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -302,12 +302,5 @@ if submitted:
         body={"values": [new_values]}
     ).execute()
     
-    # 音を鳴らす
-    st.markdown("""
-        <audio autoplay>
-            <source src="https://actions.google.com/sounds/v1/cartoon/clang_and_wobble.ogg" type="audio/ogg">
-        </audio>
-    """, unsafe_allow_html=True)
-
     st.success("保存しました！")
     st.rerun()
